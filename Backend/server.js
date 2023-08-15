2a4b85a8d1d1909beadfc8426a784e738f5807ee
kanishk <kanishkshakya1@gmail.com>
2023-08-15 16:12:37 +0530

    env Worked

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,13 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 // Config
-// Define the full path to your .env file
-const envPath = path.join(__dirname, 'Backend', 'config', 'config.env');
 
-dotenv.config({ path: envPath }); // Load environment variables from the specified path
+
+dotenv.config({ path:"Backend/config/config.env" }); 
 
 console.log(process.env.PORT)
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`);
+  console.log(`Example app listening on port ${process.env.PORT || 4000}`);
 });
 
 app.get('/', (req, res) => {
